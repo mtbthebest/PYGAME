89b7eb1bd1f81dd59a5dee2cae2a9ea9cdb5ebc3
telli_barry <tellibarry@gmail.com>
2018-03-02 19:36:00 +0900

    memory puzzle

---
+++ memory_puzzle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Memory Puzzle
+# By Al Sweigart al@inventwithpython.com
+# http://inventwithpython.com/pygame
+# Released under a "Simplified BSD" license
+
+import pygame,sys
+from pygame.locals import *
