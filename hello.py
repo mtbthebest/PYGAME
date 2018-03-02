f10b83549806dc7ae999f52a785dfe7f7e228a66
telli_barry <tellibarry@gmail.com>
2018-03-02 17:08:39 +0900

    hello

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 
-import pygame as py
+import pygame
 import sys
 from pygame.locals import *
 
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400,300))
+pygame.display.set_caption('Hello world')
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
